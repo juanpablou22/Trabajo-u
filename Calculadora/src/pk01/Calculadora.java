0c2641dca4fbf674a0b5116364928a54376e8845
Juan Palo <juanpabloum25@gmail.com>
2025-02-17 09:37:19 -0500

    Segundo comit

---
+++ Calculadora/src/pk01/Calculadora.java
@@ -0,0 +1,52 @@
+
+package pk01;
+
+
+public class Calculadora {
+
+   
+        private double numero1;
+        private double numero2;
+        private double resultado;
+    
+        public void setNumero1(double numero1) {
+            this.numero1 = numero1;
+        }
+    
+        public void setNumero2(double numero2) {
+            this.numero2 = numero2;
+        }
+    
+        public double getNumero1() {
+            return numero1;
+        }
+    
+        public double getNumero2() {
+            return numero2;
+        }
+    
+        public void calcularSuma() {
+            resultado = numero1 + numero2;
+        }
+    
+        public void calcularResta() {
+            resultado = numero1 - numero2;
+        }
+    
+        public void calcularMultiplicacion() {
+            resultado = numero1 * numero2;
+        }
+    
+        public void calcularDivision() {
+            if (numero2 != 0) {
+                resultado = numero1 / numero2;
+            } else {
+                resultado = Double.NaN; 
+            }
+        }
+    
+        public double getResultado() {
+            return resultado;
+        }
+    }
+    
---
+++ Calculadora/src/pk01/Principal.java
@@ -0,0 +1,49 @@
+
+package pk01;
+
+
+import javax.swing.JOptionPane;
+
+public class Principal {
+    public static void main(String[] args) {
+        Calculadora calc = new Calculadora();
+
+        // Ingreso de datos
+        calc.setNumero1(Double.parseDouble(JOptionPane.showInputDialog("Digite el primer número")));
+        calc.setNumero2(Double.parseDouble(JOptionPane.showInputDialog("Digite el segundo número")));
+
+        int opcion;
+        do {
+            String menu = "Seleccione una operación:\n"
+                    + "1. Suma\n"
+                    + "2. Resta\n"
+                    + "3. Multiplicación\n"
+                    + "4. División\n"
+                    + "5. Salir";
+            opcion = Integer.parseInt(JOptionPane.showInputDialog(menu));
+
+            if (opcion == 1) {
+                calc.calcularSuma();
+                JOptionPane.showMessageDialog(null, "La suma es: " + calc.getResultado());
+            } else if (opcion == 2) {
+                calc.calcularResta();
+                JOptionPane.showMessageDialog(null, "La resta es: " + calc.getResultado());
+            } else if (opcion == 3) {
+                calc.calcularMultiplicacion();
+                JOptionPane.showMessageDialog(null, "La multiplicación es: " + calc.getResultado());
+            } else if (opcion == 4) {
+                if (calc.getNumero2() != 0) {
+                    calc.calcularDivision();
+                    JOptionPane.showMessageDialog(null, "La división es: " + calc.getResultado());
+                } else {
+                    JOptionPane.showMessageDialog(null, "No se puede dividir entre 0.");
+                }
+            } else if (opcion == 5) {
+                JOptionPane.showMessageDialog(null, "Saliendo");
+            }
+            {
+
+            }
+        } while (opcion != 5);
+    }
+}
