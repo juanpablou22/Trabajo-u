356047a08d70fe7d4176f936fce7af17604000ff
Juan Palo <juanpabloum25@gmail.com>
2025-02-17 10:13:27 -0500

    Tercer comit

---
+++ Contador_Consonantes_Vocales/src/pk01/Principal.java
@@ -0,0 +1,14 @@
+
+package pk01;
+import javax.swing.JOptionPane;
+public class Principal {
+    public static void main(String[] args) {
+        String texto = JOptionPane.showInputDialog("Ingrese una palabra o frase:");
+        Contador contador = new Contador(texto); // Crear objeto
+        contador.contarLetras(); // Contar vocales y consonantes
+
+        JOptionPane.showMessageDialog(null, "Vocales: " + contador.getVocales() +
+                "\nConsonantes: " + contador.getConsonantes());
+    }
+}
+
